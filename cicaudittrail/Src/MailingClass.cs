ee33e2117352f9d593ed48ee687c472b98a31695
hyogataf <ahad.fall@cbao.sn>
2014-11-14 17:48:29 +0000

    Receiving mail

--- cicaudittrail/Src/MailingClass.cs
+++ cicaudittrail/Src/MailingClass.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Net.Mail;
 using System.Configuration;
+using Newtonsoft.Json;
+using ActiveUp.Net.Mail;
+using cicaudittrail.Models;
+using System.Diagnostics;
 //using OpenPop.Pop3;
 
 namespace cicaudittrail.Src
@@ -53,8 +57,45 @@ namespace cicaudittrail.Src
             {
                 CommunicationWSUri = CommunicationWSUri + appname; // ajout du parametre 'appname' de la methode GET
                 System.Net.WebClient webClient = new System.Net.WebClient();
-                string dwml = webClient.DownloadString(CommunicationWSUri);
+                string download = webClient.DownloadString(CommunicationWSUri);
 
+                List<Message> mails = JsonConvert.DeserializeObject<List<Message>>(download);
+                CicMessageMailRepository CicMessageMailRepository = new CicMessageMailRepository();
+
+                for (int i = 0; i < mails.Count; i++) // Loop through List with for
+                {
+                    var msg = mails[i];
+
+                    Debug.WriteLine("msg From = " + msg.From.ToString());
+                    Debug.WriteLine("msg Sender = " + msg.Sender.ToString());
+                    Debug.WriteLine("msg Subject = " + msg.Subject);
+
+                    CicMessageMail CicMessageMail = new CicMessageMail();
+                    CicMessageMail.DateMessage = DateTime.Now;
+                    CicMessageMail.MessageContent = msg.BodyHtml.Text;
+                    CicMessageMail.ObjetMessage = msg.Subject;
+                    CicMessageMail.Sens = Sens.I.ToString();
+                    CicMessageMail.UserMessage = msg.From.Name + "<" + msg.From.Email + ">";
+
+                    //TODO recup CicRequestResultsFollowedId selon le msg.Subject
+                    CicMessageMailRepository.InsertOrUpdate(CicMessageMail);
+                    CicMessageMailRepository.Save();
+
+                    //Gestion des pieces jointes
+                    CicMessageMailDocumentsRepository CicMessageMailDocumentsRepository = new CicMessageMailDocumentsRepository();
+                    foreach (MimePart attachment in msg.Attachments)
+                    {
+                        CicMessageMailDocuments CicMessageMailDocuments = new CicMessageMailDocuments();
+                        CicMessageMailDocuments.CicMessageMailId = CicMessageMail.CicMessageMailId;
+                        CicMessageMailDocuments.DateCreated = DateTime.Now;
+                        CicMessageMailDocuments.DocumentName = attachment.ContentName;
+                        CicMessageMailDocuments.DocumentType = attachment.ContentType.MimeType;
+                        CicMessageMailDocuments.Document = attachment.BinaryContent;
+                        CicMessageMailDocumentsRepository.InsertOrUpdate(CicMessageMailDocuments);
+                        CicMessageMailDocumentsRepository.Save();
+                    }
+
+                }
 
             }
 
